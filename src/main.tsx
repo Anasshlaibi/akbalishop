de01503541363c1af32428c2041969881ccc5211
Anasshlaibi <anasshlaibi@gmail.com>
2026-08-11 13:45:24 +0100

    fix: resolve provider order in main.tsx to eliminate white screen error

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { CartProvider } from './context/CartContext';
-import { ShopProvider } from './context/ShopContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CartProvider>
-      <ShopProvider>
-        <App />
-      </ShopProvider>
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
